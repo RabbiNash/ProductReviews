9b2a2cc7c754aea014fb0e3fcc28ddecde395ce6
RabbiNash <tmakuti@icloud.com>
2021-05-08 03:09:54 +0200

    Fix stub error

--- domain/src/test/java/dev/nashe/domain/DataStub.kt
+++ domain/src/test/java/dev/nashe/domain/DataStub.kt
@@ -11,7 +11,7 @@ internal object DataStub {
 
     val fakeProduct = Product(
         currency = "euro",
-        price = 5,
+        price = 5.0,
         id = "HI347",
         name = "product",
         imgUrl = "https://assets.adidas.com/images/w_320,h_320,f_auto,q_auto:sensitive,fl_lossy/0fcb1719675b4df0a160ab1a0104630b_9366/GD3509_01_laydown.jpg",
